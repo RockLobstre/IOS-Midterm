7857c5be843a6ab01c50f2eb65516630425a4625
matin <hsalehi91@gmail.com>
2018-10-18 19:04:36 -0400

    Initial Commit

---
+++ IOS-Midterm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IOS-Midterm
+//
+//  Created by Matin Salehi on 2018-10-18.
+//  Copyright © 2018 Matin Salehi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
